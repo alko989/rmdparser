62b4d0200ebb2b157347291f01fe512fea1528f1
Alexandros Kokkalis <alko@aqua.dtu.dk>
2023-11-02 02:19:58 +0100

    Add simple lexer and tests

---
+++ rmdparser/lexer.py
@@ -0,0 +1,207 @@
+"""
+Lexer for Rmarkdown files that use Pandoc's markdown flavour.
+
+Inline formating
+ - italics (_text_ or *text*)
+ - bold (__text__ or **text**)
+ - subscript (H~2~O)
+ - superscript (s^2)
+ - code(`print("Hello, World!")`)
+ - links ([text](link))
+ - images (![alt text](path/to/image))
+ - footnote (^[I am a footnote])
+Block-level elements
+ - heading (### Heading level 3)
+ - unordered list ( - list item 1)
+ - ordered list ( 1. list item )
+ - code (```Code block```)
+ - block quote (> "First line
+                  Second line"
+                > --- Mark Twain)
+Special tokens
+ - EOF
+ - newline
+
+"""
+
+
+from enum import Enum
+
+
+class Token(Enum):
+    """Enumerate possible token types."""
+
+    EOF = 0
+    ILLEGAL = 1
+    NEWLINE = 2
+    TEXT = 3
+    ITALICS = 4
+    STRONG = 5
+    BLOCKQUOTE = 6
+    SUPERSCRIPT = 7
+    SUBSCRIPT = 8
+    ULIST = 9
+    OLIST = 10
+    LINK = 11
+    IMAGE = 12
+    CODE = 13
+    INLINECODE = 14
+    HEADER1 = 15
+    HEADER2 = 16
+    HEADER3 = 17
+
+
+class NewToken(object):
+    """Describes a single token."""
+
+    def __init__(self, _type: Enum,
+                 _literal: str = "", _line: int = 1) -> None:
+        """Make Token object string `value`, one of the class constants."""
+        self.type = _type
+        self.literal = _literal
+        self.line = _line
+
+    def __repr__(self):
+        """Print a token."""
+        print(f"Type: {self.type}, line: {self.line}, literal: {self.literal}")
+
+    def __str__(self):
+        return f"Type: {self.type}, line: {self.line}, literal: {self.literal}"
+
+    def __eq__(self, o):
+        """Define equality as equal type, line and literal."""
+        return self.type == o.type and \
+            self.line == o.line and \
+            self.literal == o.literal
+
+
+def stripchars(s, chars):
+    """Strip `chars` from the beginning of a string s."""
+    if len(s) < 1:
+        return s
+    i = 0
+    while s[i] in chars:
+        print(s[i])
+        i += 1
+    return s[i:]
+
+
+class Lexer(object):
+    """Do lexical analysis of a string and get tokens."""
+
+    def __init__(self, _input: str,
+                 linestart: bool = True, position: int = 0,
+                 readPosition: int = 0, ch: str = "",
+                 startofline: bool = True) -> None:
+        """Make Lexer object with string input."""
+        self.input = _input
+        self.position = position
+        self.readPosition = readPosition
+        self.line = 1
+        self.startofline = startofline
+        self.ch = _input[readPosition]
+
+    def nextToken(self) -> Token:
+        """Get the next token based on rules."""
+        self.readChar()
+        res = Token.ILLEGAL
+        lit = self.ch
+        line = self.line
+
+        la1 = self.peakahead(0)
+        la2 = self.peakahead(1)
+        la3 = self.peakahead(2)
+
+        if self.ch == "\n":
+            res = Token.NEWLINE
+            self.line += 1
+        elif self.ch == "#":
+            if la1 == " ":
+                res = Token.HEADER1
+                lit = self.get_until_chars(offset=1)
+            elif la1 == "#" and la2 == " ":
+                res = Token.HEADER2
+                lit = self.get_until_chars(offset=2)
+            elif la1 == "#" and la2 == "#" and la3 == " ":
+                res = Token.HEADER3
+                lit = self.get_until_chars(offset=3)
+            else:
+                res = Token.TEXT
+                lit = self.get_until_chars(offset=-1)
+        elif self.ch in "0123456789" and la1 == ".":
+            res = Token.OLIST
+            lit = self.get_until_chars(offset=2)
+        elif (self.ch == "-" and la1 != "-") or \
+             (self.ch == "*" and la1 != "*"):
+            res = Token.ULIST
+            lit = self.get_until_chars(offset=1)
+        elif self.ch == ">":
+            res = Token.BLOCKQUOTE
+            lit = self.get_until_chars(offset=1)
+            lit = stripchars(lit, [" ", ">"])
+        elif self.ch == "\0":
+            res = Token.EOF
+            lit = "\0"
+        elif self.ch == "`":
+            mult = 1
+            i = self.readPosition + 1
+            while self.input[i] == "`":
+                mult += 1
+                i += 1
+            print(f"Found {mult} `")
+            res = Token.CODE
+            lit = self.get_until_chars(offset=mult, chars="`", multiples=mult)
+            self.readChar()
+            print("Done", self.ch)
+        else:
+            res = Token.TEXT
+            lit = self.get_until_chars(offset=-1)
+
+        return NewToken(res, lit.strip(), line)
+
+    def peakahead(self, pos: int = 0):
+        """Get a char ahead of readPosition if there is one."""
+        if (self.readPosition + pos >= len(self.input)):
+            return "\0"
+        else:
+            return self.input[self.readPosition + pos]
+
+    def get_until_chars(self, offset: int = 0, chars="\n", multiples=1):
+        """
+        Give me the rest of the letters until newline.
+        TODO: Cleanupy
+        """
+        self.readPosition += offset
+        self.position += offset
+        res = ""
+        self.readChar()
+        while self.ch != "\0":
+            if self.ch == chars:
+                found = 1
+                for i in range(multiples - 1):
+                    if self.peakahead() == chars:
+                        found += 1
+                        self.readChar()
+                    else:
+                        break
+                if found == multiples:
+                    break
+                else:
+                    res += chars * found
+                    self.readChar()
+            else:
+                res += self.ch
+                self.readChar()
+        self.line += res.count("\n")
+        if chars == "\n":
+            self.line += 1
+        return res
+
+    def readChar(self):
+        """Read the next character and move positions ahead."""
+        if self.readPosition >= len(self.input):
+            self.ch = "\0"
+        else:
+            self.ch = self.input[self.readPosition]
+        self.position = self.readPosition
+        self.readPosition += 1
---
+++ tests/test_lexer.py
@@ -0,0 +1,226 @@
+from rmdparser.lexer import NewToken, Token, Lexer
+
+
+def test_Token():
+    tok = NewToken(Token.HEADER2, "Hello, World!", 313)
+    assert tok.type == Token.HEADER2
+    assert tok.line == 313
+    assert tok.literal == "Hello, World!"
+
+
+def test_Header1():
+    input = "This is just text\n\n# This is a heading level one"
+    expected = [
+        NewToken(Token.TEXT, "This is just text", 1),
+        NewToken(Token.NEWLINE, "", 2),
+        NewToken(Token.HEADER1, "This is a heading level one", 3)
+    ]
+
+    lex = Lexer(input)
+
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        print(tok.type)
+        print(ex.type)
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+
+def test_Blockquote():
+    input = "> This is a blqt\n\n# This is a heading level one"
+    expected = [
+        NewToken(Token.BLOCKQUOTE, "This is a blqt", 1),
+        NewToken(Token.NEWLINE, "", 2),
+        NewToken(Token.HEADER1, "This is a heading level one", 3)
+    ]
+
+    lex = Lexer(input)
+
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        print(tok.type)
+        print(ex.type)
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+
+def test_FakeBlockquote():
+    input = "> \n >\n>> sadsad\n# This is a heading level one"
+    expected = [
+        NewToken(Token.BLOCKQUOTE, "", 1),
+        NewToken(Token.TEXT, ">", 2),
+        NewToken(Token.BLOCKQUOTE, "sadsad", 3),
+        NewToken(Token.HEADER1, "This is a heading level one", 4)
+    ]
+
+    lex = Lexer(input)
+
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        print(tok.type)
+        print(ex.type)
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+
+def test_MixHeaders():
+    input = "This is just text\n\n# This is a heading level one\n" \
+        "## This is level 2\n\n\n" \
+        "This is a small text\nThis is just text## This is not 2" \
+        "\nThis is text\n\n### And this is level 3"
+    expected = [
+        NewToken(Token.TEXT, "This is just text", 1),
+        NewToken(Token.NEWLINE, "", 2),
+        NewToken(Token.HEADER1, "This is a heading level one", 3),
+        NewToken(Token.HEADER2, "This is level 2", 4),
+        NewToken(Token.NEWLINE, "", 5),
+        NewToken(Token.NEWLINE, "", 6),
+        NewToken(Token.TEXT, "This is a small text", 7),
+        NewToken(Token.TEXT, "This is just text## This is not 2", 8),
+        NewToken(Token.TEXT, "This is text", 9),
+        NewToken(Token.NEWLINE, "", 10),
+        NewToken(Token.HEADER3, "And this is level 3", 11)
+    ]
+
+    lex = Lexer(input)
+
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        print(f"is {tok.type} expected: {ex.type}")
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+
+def test_Newline():
+    input = "\n\n\n"
+    expected = [
+        NewToken(Token.NEWLINE, "", 1),
+        NewToken(Token.NEWLINE, "", 2),
+        NewToken(Token.NEWLINE, "", 3)
+    ]
+
+    lex = Lexer(input)
+
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+
+def test_olist():
+    input = "1. first\n2. second\n3. third"
+    expected = [
+        NewToken(Token.OLIST, "first", 1),
+        NewToken(Token.OLIST, "second", 2),
+        NewToken(Token.OLIST, "third", 3)
+    ]
+
+    lex = Lexer(input)
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+
+def test_ulist():
+    input = "- first\n- second\n- third"
+    expected = [
+        NewToken(Token.ULIST, "first", 1),
+        NewToken(Token.ULIST, "second", 2),
+        NewToken(Token.ULIST, "third", 3)
+    ]
+
+    lex = Lexer(input)
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+
+def test_ulist2():
+    input = "* first\n- second\n* third\n- fourth"
+    expected = [
+        NewToken(Token.ULIST, "first", 1),
+        NewToken(Token.ULIST, "second", 2),
+        NewToken(Token.ULIST, "third", 3),
+        NewToken(Token.ULIST, "fourth", 4)
+    ]
+
+    lex = Lexer(input)
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+
+def test_blockcode():
+    input = "```def get_stuff(arg1: int = 0, arg2: str = 'asd'):\n" \
+        "    print(arg1)\n" \
+        "    print(arg2)```\n# Heading 1"
+    expected = [
+        NewToken(Token.CODE, "def get_stuff(arg1: int = 0, "
+                 "arg2: str = 'asd'):\n"
+                 "    print(arg1)\n    print(arg2)", 1),
+        NewToken(Token.NEWLINE, "", 3),
+        NewToken(Token.HEADER1, "Heading 1", 4)
+    ]
+
+    lex = Lexer(input)
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        print(tok)
+        print(ex)
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+
+def test_blockcode5():
+    input = "`````def get_stuff(arg1: int = 0, arg2: str = 'asd'):\n" \
+        "    print(arg1)\n" \
+        "    print(arg2)`````\n# Heading 1"
+    expected = [
+        NewToken(Token.CODE, "def get_stuff(arg1: int = 0, "
+                 "arg2: str = 'asd'):\n"
+                 "    print(arg1)\n    print(arg2)", 1),
+        NewToken(Token.NEWLINE, "", 3),
+        NewToken(Token.HEADER1, "Heading 1", 4)
+    ]
+
+    lex = Lexer(input)
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        print(tok)
+        print(ex)
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
+
+def test_blockcode_with__inside__backticks():
+    input = "```def get_stuff(arg1: int = 0, arg2: str = 'asd'):\n" \
+        "    print(arg1)\n" \
+        "    print(`arg2`)```\n# Heading 1"
+    expected = [
+        NewToken(Token.CODE, "def get_stuff(arg1: int = 0, "
+                 "arg2: str = 'asd'):\n"
+                 "    print(arg1)\n    print(`arg2`)", 1),
+        NewToken(Token.NEWLINE, "", 3),
+        NewToken(Token.HEADER1, "Heading 1", 4)
+    ]
+
+    lex = Lexer(input)
+    for i, ex in enumerate(expected):
+        tok = lex.nextToken()
+        print(tok)
+        print(ex)
+        assert tok.type == ex.type
+        assert tok.literal == ex.literal
+        assert tok.line == ex.line
